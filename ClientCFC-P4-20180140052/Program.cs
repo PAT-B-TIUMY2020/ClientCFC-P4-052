d40f9c6c1671a8c5379c09775e55330da4c219c8
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-10-24 09:32:19 +0700

    First Commit

--- ClientCFC-P4-20180140052/Program.cs
+++ ClientCFC-P4-20180140052/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140052;
 
 namespace ClientCFC_P4_20180140052
 {
@@ -10,6 +12,20 @@ namespace ClientCFC_P4_20180140052
     {
         static void Main(string[] args)
         {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+           ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            
         }
     }
 }
